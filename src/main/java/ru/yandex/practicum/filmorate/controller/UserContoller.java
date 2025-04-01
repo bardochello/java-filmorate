a5014f58d1bd025f2799cdeb64d1cd181fe5bc8e
Apti <aptigardanov@ya.ru>
2025-04-02 00:13:27 +0300

    Исправления после код-ревью №1

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserContoller.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserContoller.java
@@ -4,11 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.utils.ValidationUtils;
-
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
