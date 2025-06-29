3db788168cc6089e1756f2a1ac2df40192c85b80
Apti <aptigardanov@ya.ru>
2025-06-30 01:40:12 +0300

    sprint 12 fix#4

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
