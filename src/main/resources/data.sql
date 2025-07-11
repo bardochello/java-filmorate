975cf13a8ba243c35f105d558849f9d83493c49a
Apti <aptigardanov@ya.ru>
2025-07-12 02:51:54 +0300

    sprint 12 fix #2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 DELETE FROM mpa;
-DELETE FROM genres;
+INSERT INTO mpa (id, name) VALUES (1, 'G'), (2, 'PG'), (3, 'PG-13'), (4, 'R'), (5, 'NC-17');
 
-INSERT INTO mpa (name) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
-INSERT INTO genres (name) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
+DELETE FROM genres;
+INSERT INTO genres (id, name) VALUES (1, 'Комедия'), (2, 'Драма'), (3, 'Мультфильм'), (4, 'Триллер'),
+    (5, 'Документальный'), (6, 'Боевик');
